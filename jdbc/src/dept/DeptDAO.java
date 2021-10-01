4203ed1caf94cc20f493e7d2f785be6e18c64d2b
jung <3dnr22@gmail.com>
2021-10-01 12:56:47 +0900

    메인화면 완성

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,6 +1,5 @@
 package dept;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -35,17 +34,17 @@ public class DeptDAO {
 
 	// 부서정보 전체 가져오기 select * from dept_temp;
 	public List<DeptDTO> getRows() {
-		Connection con =  null;
+		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		List<DeptDTO> list =  new ArrayList<DeptDTO>();
+		List<DeptDTO> list = new ArrayList<DeptDTO>();
 		try {
 			con = getConnection();
-			String sql="select * from dept_temp";
+			String sql = "select * from dept_temp";
 			pstmt = con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				DeptDTO dto = new DeptDTO();
 				dto.setDeptNo(rs.getInt("deptno"));
 				dto.setDname(rs.getString("dname"));
@@ -60,8 +59,7 @@ public class DeptDAO {
 				pstmt.close();
 				con.close();
 			} catch (Exception e2) {
-				
-				
+
 			}
 		}
 		return list;
@@ -78,10 +76,10 @@ public class DeptDAO {
 			con = getConnection();
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, deptno);
-			rs =  pstmt.executeQuery();
-			
-			if(rs.next()) {
-				dto= new DeptDTO();
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new DeptDTO();
 				dto.setDeptNo(rs.getInt(1));
 				dto.setDname(rs.getString(2));
 				dto.setLoc(rs.getString(3));
@@ -99,69 +97,93 @@ public class DeptDAO {
 		}
 		return dto;
 	}
-	
+
 	public boolean insert(DeptDTO dto) {
 		boolean flag = false;
-		
+
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		
+
 		try {
 			con = getConnection();
-			String sql = "insert into dept_temp(deptno,dename,loc) values(?,?,?)";
+			String sql = "insert into dept_temp(deptno,dname,loc) values(?,?,?)";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, dto.getDeptNo());
 			pstmt.setString(2, dto.getDname());
 			pstmt.setString(3, dto.getLoc());
 			int result = pstmt.executeUpdate();
-			if(result>0) {
+			if (result > 0) {
 				flag = true;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
-				
+
 			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
 		}
 		return flag;
 	}
-	//부서명을 변경하는 작업 update dept_temp set dname=? where deptno=?
-	   public boolean updateSet(String dname,int deptNo) {
-	      
-	   Connection con = null;
-	   PreparedStatement pstmt = null;
-	   boolean updateflag = false;
-	   
-	   try {
-	      con = getConnection();
-	      String sql = "update dept_temp set dname=? where deptNo=?";
-	      pstmt = con.prepareStatement(sql);
-	      pstmt.setString(1, dname);
-	      pstmt.setInt(2, deptNo);
-	      
-	      
-	      int result = pstmt.executeUpdate();
-	      if(result >0) {
-	         updateflag = true;
-	      }
-	   } catch (Exception e) {
-	      e.printStackTrace();
-	   }finally {
-	      try {
-	         pstmt.close();
-	         con.close();
-	         
-	      } catch (Exception e2) {
-	         e2.printStackTrace();
-	      }
-	   }
-	return updateflag;
-	   
+
+	// 부서명을 변경하는 작업 update dept_temp set dname=? where deptno=?
+	public boolean updateSet(String dname, int deptNo) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean updateflag = false;
+
+		try {
+			con = getConnection();
+			String sql = "update dept_temp set dname=? where deptNo=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, dname);
+			pstmt.setInt(2, deptNo);
+
+			int result = pstmt.executeUpdate();
+			if (result > 0) {
+				updateflag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				con.close();
+
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return updateflag;
+
 	}
-	   
-	   
+
+	public boolean deletex(int deptNo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean deleteflag = false;
+		try {
+			con = getConnection();
+			String sql = "delete from dept_temp where deptno=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, deptNo);
+			int result = pstmt.executeUpdate();
+			if (result > 0) {
+				deleteflag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return deleteflag;
+	}
 
 }
--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,16 +1,90 @@
 package dept;
 
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.text.html.parser.DTD;
 
 public class DeptMain {
 
 	public static void main(String[] args) {
-		
+
 		boolean run = true;
 		Scanner sc = new Scanner(System.in);
-		
-		while(run) {
-			
+		DeptDAO dao = new DeptDAO();
+
+		while (run) {
+			System.out.println("***********************");
+			System.out.println("1. 전체 부서 조회");
+			System.out.println("2. 특정 부서 조회");
+			System.out.println("3. 부서 추가");
+			System.out.println("4. 부서 수정");
+			System.out.println("5. 부서 삭제");
+			System.out.println("6. 종료");
+			System.out.println("***********************");
+
+			System.out.print("선택 >>");
+			int menu = sc.nextInt();
+
+			switch (menu) {
+			case 1:
+				List<DeptDTO> list = dao.getRows();
+
+				System.out.println("부서번호\t부서명\t지역");
+				for (DeptDTO dto : list) {
+					System.out.print(dto.getDeptNo() + "\t");
+					System.out.print(dto.getDname() + "\t");
+					System.out.print(dto.getLoc() + "\t");
+					System.out.println();
+				}
+				System.out.println();
+				break;
+			case 2:
+				System.out.println("\n조회 부서 입력>>");
+				int deptno = sc.nextInt();
+				DeptDTO dao1 =  dao.getrow(deptno);
+				System.out.println("조회 내용은 다음과 같습니다.");
+				System.out.println("부서번호: "+dao1.getDeptNo());
+				System.out.println("부서명: "+dao1.getDname());
+				System.out.println("부위치: "+dao1.getLoc());
+				System.out.println();
+				break;
+			case 3:
+				System.out.println("부서 추가");
+				System.out.print("부서번호 : ");
+				int newdepNo = sc.nextInt();
+				System.out.print("부서명 : ");
+				String newDname =  sc.next();
+				System.out.print("부서위치 : ");
+				String newLoc = sc.next();
+				DeptDTO dto = new DeptDTO(newdepNo,newDname,newLoc);
+				System.out.println(dao.insert(dto)?"부서입력 성공":"부서입력 실패");
+				System.out.println();
+				break;
+			case 4:
+				System.out.println("\n부서 수정");
+				System.out.print("\n수정 부서 번호 : ");
+				int updateDeptNo = sc.nextInt();
+				System.out.print("수정 부서명 : ");
+				String updateDname = sc.next();
+				
+				System.out.println(dao.updateSet(updateDname, updateDeptNo)?"부서 수정 성공": "실패");
+				System.out.println();
+				break;
+			case 5:
+				System.out.println("\n부서 삭제");
+				System.out.print("\n삭제 부서 번호 : ");
+				int removeNo = sc.nextInt();
+				System.out.println(dao.deletex(removeNo)?"삭제 성공":"실패");
+				break;
+			case 6:
+				run = false;
+				break;
+
+			default:
+				System.out.println("번호를 확인해 주세요.");
+				break;
+			}
 		}
 
 	}
