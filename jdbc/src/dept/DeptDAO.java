ec19e52fb5012c860f29bb8629e17af01f46e12d
jung <3dnr22@gmail.com>
2021-10-01 11:15:18 +0900

    dd

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class DeptDAO {
 	// empDAO static, Connection
-	int asd =1111;
+
 	static {
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
@@ -162,30 +162,6 @@ public class DeptDAO {
 	   
 	}
 	   
-	   public boolean deletex(int deptNo) {
-		      Connection con = null;
-		      PreparedStatement pstmt = null;
-		      boolean deleteflag = false;
-		      try {
-		         con = getConnection();
-		         String sql = "delete from dept_temp where deptno=?";
-		         pstmt = con.prepareStatement(sql);
-		         pstmt.setInt(1, deptNo);
-		         int result = pstmt.executeUpdate();
-		         if(result>0) {
-		            deleteflag = true;
-		         }
-		      } catch (Exception e) {
-		         e.printStackTrace();
-		      }finally {
-		      try {
-		         pstmt.close();
-		         con.close();
-		      } catch (Exception e2) {
-		         e2.printStackTrace();
-		         }   
-		      }
-		      return deleteflag;
-		   }
+	   
 
 }
